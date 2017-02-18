493fd04ebae6c10562ead26bf9898fdf7d67e447
kr3110 <n.nkr3110@gmail.com>
2017-02-18 07:47:51 -0500

    creating models folder for pr

--- Api/index.js
+++ Api/index.js
@@ -1,31 +1,28 @@
 'use strict'
-
-var express=require('express');
-var router=express.Router();
-var passport=require('passport');
-var mongoose=require('mongoose')
-var jwt=require('express-jwt');
-var jwkRsa=require('jwks-rsa');
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var mongoose = require('mongoose')
+var jwt = require('express-jwt');
+var jwkRsa = require('jwks-rsa');
 //
-
 const jwtCheck = jwt({
-  // Dynamically provide a signing key based on the kid in the header and the singing keys provided by the JWKS endpoint.
-  secret: jwkRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `ecobik.auth0.com/.well-known/jwks.json`
-  }),
-
-  // Validate the audience and the issuer.
-  audience: '2YGDWSgclvWwDvZ2ScGtW3wFbFkCCTBL',
-  issuer: `ecobik.auth0.com`,
-  algorithms: ['RS256']
+    // Dynamically provide a signing key based on the kid in the header and the singing keys provided by the JWKS endpoint.
+    secret: jwkRsa.expressJwtSecret({
+        cache: true
+        , rateLimit: true
+        , jwksRequestsPerMinute: 5
+        , jwksUri: `ecobik.auth0.com/.well-known/jwks.json`
+    }), // Validate the audience and the issuer.
+    audience: '2YGDWSgclvWwDvZ2ScGtW3wFbFkCCTBL'
+    , issuer: `ecobik.auth0.com`
+    , algorithms: ['RS256']
 });
 //
-router.use('/hi',jwtCheck,function(req,res){
-    
-    
-});
-
-module.exports=router;
+router.use('/hi', jwtCheck, function (req, res) {});
+router.get('/', function (req, res) {});
+router.get('/', function (req, res) {});
+router.get('/', function (req, res) {});
+router.get('/', function (req, res) {});
+router.get('/', function (req, res) {});
+module.exports = router;
